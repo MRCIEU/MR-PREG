b89df0d4e9189437997aa70539f2ecf295543482
Alba Fernández-Sanlés, PhD <albafsanles@gmail.com>
2021-12-06 16:56:37 +0000

    Create script.sh

---
+++ BiB/GWAS/1.vcf2bgen/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#PBS -N bgen_job
+#PBS -j oe
+#PBS -o bib1-22.out
+#PBS -q himem
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=48:00:00
+#PBS -t 1-22
+
+module add apps/qctool-2.1
+
+echo "Current working directory is `pwd`" or $PBS_O_WORKDIR
+echo "Running on `hostname`" 
+echo job ID is $PBS_JOBID 
+echo "But output is bib1-22.out"
+echo This jobs runs on nodes `cat $PBS_NODEFILE | uniq`
+echo BC3 has 16 high-memory nodes, each of which has 256GB of RAM. We request 16 processors in a single compute node. 
+echo count the number of processors available: numprocs=`wc $PBS_NODEFILE | awk '{print $1}'`
+echo "job started at `date`"
+
+qctool -g path/to/data_chr0${PBS_ARRAYID}.vcf.gz -og path/to/data_chr0${PBS_ARRAYID}_bib.bgen -os path/to/sampleid_bib0${PBS_ARRAYID}.sample -sex-column sex -bgenix -index -g path/to
+bgenix -index -g path/to/data_chr0${PBS_ARRAYID}_bib.bgen
+
+echo "job finished at `date`"
