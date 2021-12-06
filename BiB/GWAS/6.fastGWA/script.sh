33e0bee4b0353d4a3cc60c23b1e2ef75cbdf84c8
Alba Fernández-Sanlés, PhD <albafsanles@gmail.com>
2021-12-06 17:05:02 +0000

    Create script.sh

---
+++ BiB/GWAS/6.fastGWA/script.sh
@@ -0,0 +1,4 @@
+## GRAMMAR-Gamma is an approximate method that gives O(MN) time of SNP testing instead of O(MN2) 
+##(M is the number of SNPs, N - that of individuals)
+
+--fastGWA-mlm --data --est-vg --h2-limit 1.6 --save-fastGWA-mlm-residual --seed 123
