403ca417793bc7e60c241e2d21419c341412dd5b
Nance <45036800+nm15336@users.noreply.github.com>
2021-12-07 11:10:25 +0000

    Update script.sh

--- BiB/GWAS/2.grm/script.sh
+++ BiB/GWAS/2.grm/script.sh
@@ -12,7 +12,7 @@
 module add apps/gcta/1.92
 
 gcta --bgen /mnt/storage/private/mrcieu/data/bib/genetic/variants/arrays/imputed/uk10k_1000genomes/released/2017-01-27/data/bgen/tom/data_chr${SLURM_ARRAY_TASK_ID}.bgen \
---make-grm-part 5 1 \
+--make-grm-part 50 ${SLURM_ARRAY_TASK_ID} \
 --thread-num 5 \
 --maf 0.01 \
 --out path/to/bib_cal_cleaned_maf_0_01
