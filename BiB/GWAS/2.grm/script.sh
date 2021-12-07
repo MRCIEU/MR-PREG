cc1f123c043b60e40d16b63bb4017079d8376f3d
Nance <45036800+nm15336@users.noreply.github.com>
2021-12-07 11:00:21 +0000

    Update script.sh

--- BiB/GWAS/2.grm/script.sh
+++ BiB/GWAS/2.grm/script.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-	
-	#PBS -N grm_job
-	#PBS -j oe
-	#PBS -o bib1-22.out
-	#PBS -q himem
-	#PBS -l nodes=1:ppn=16
-	#PBS -l walltime=48:00:00
-	#PBS -t 1-22
-	
+
+#SBATCH --job-name=make-grm
+#SBATCH --partition=cpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:0:0
+#SBATCH --mem=50gb
+#SBATCH --array=1-22
+
 module add apps/gcta/1.92
 
-gcta --bgen path/to/data_chr0${PBS_ARRAYID}.bgen \
---make-grm-part 5 $${PBS_ARRAYID} --thread-num 5 --maf 0.01 --out ~/grm/bib_cal _cleaned_maf_0_01
+gcta --bgen /mnt/storage/private/mrcieu/data/bib/genetic/variants/arrays/imputed/uk10k_1000genomes/released/2017-01-27/data/bgen/tom/data_chr${SLURM_ARRAY_TASK_ID}.bgen \
+--make-grm-part 5 1 \
+--thread-num 5 \
+--maf 0.01 \
+--out path/to/bib_cal_cleaned_maf_0_01
+
+
