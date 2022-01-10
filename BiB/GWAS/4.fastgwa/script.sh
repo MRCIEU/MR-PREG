e613f4a878c8c1835ad51acf3bf8b92f4bba145d
Alba Fernández-Sanlés, PhD <albafsanles@gmail.com>
2022-01-10 14:02:09 +0000

    Update script.sh

--- BiB/GWAS/4.fastgwa/script.sh
+++ BiB/GWAS/4.fastgwa/script.sh
@@ -2,3 +2,9 @@
 ##(M is the number of SNPs, N - that of individuals)
 
 --fastGWA-mlm --data --est-vg --h2-limit 1.6 --save-fastGWA-mlm-residual --seed 123
+
+
+#run in the same sample
+--fastGWA-lr linear regression
+--fastGWA-mlm linear regression
+#Generate histogram of pvals to see if the distribution is like Tom’s (normal) or uniform (as expected) – check with GWAS inspector package 
