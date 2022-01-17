2d12d3896c965b1e42d1ea1f55b2ef7d83da8990
Alba Fernández-Sanlés, PhD <albafsanles@gmail.com>
2022-01-17 15:09:57 +0000

    Update script.sh

--- BiB/GWAS/4.fastgwa/script.sh
+++ BiB/GWAS/4.fastgwa/script.sh
@@ -1,7 +1,21 @@
 ## GRAMMAR-Gamma is an approximate method that gives O(MN) time of SNP testing instead of O(MN2) 
 ##(M is the number of SNPs, N - that of individuals)
 
---fastGWA-mlm --data --est-vg --h2-limit 1.6 --save-fastGWA-mlm-residual --seed 123
+#!/bin/bash
+
+#SBATCH --job-name=test_job
+#SBATCH --partition=test
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --array=1-22
+
+module load apps/gcta/1.93.2-beta
+
+gcta64 --bgen /bp1/mrcieu1/data/bib/genetic/variants/arrays/imputed/uk10k_1000genomes/released/2017-01-27/data/bgen/tom/data_chr${SLURM_ARRAY_TASK_ID}.bgen --grm-sparse sp_grm --fastGWA-lr --pheno /user/home/rt20627/pheno.txt --out testrun
+## we got an error of not finding the pheno file and we suspect that we need to specigy the grms (--grm-sparse "sp_grm" to be changed). we tried by reordering the columns of the pheno file and that did not work
+
+#--fastGWA-mlm --data --est-vg --h2-limit 1.6 --save-fastGWA-mlm-residual --seed 123
 
 
 #run in the same sample
