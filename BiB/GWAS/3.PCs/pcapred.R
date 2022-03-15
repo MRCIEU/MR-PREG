fe76baa3a087c316a42f65d51d5359c9d72e7dc9
Marwa AL Arab <cu20932@bc4login1.bc4.acrc.priv>
2022-03-15 13:43:34 +0000

    pcapred R code change

--- BiB/GWAS/3.PCs/pcapred.R
+++ BiB/GWAS/3.PCs/pcapred.R
@@ -1,14 +1,17 @@
+#Marwa Al Arab
+#generate UKB pcs after merging the local data (bib) with reference data (see https://github.com/danjlawson/pcapred for documentation)
 #remotes::install_github("danjlawson/pcapred.largedata")
 #remotes::install_github("danjlawson/pcapred")
 
 library(pcapred)
 library(pcapred.largedata)
-
+#get the location of the data from linux environment variables
+imputed_data<-(getenv "bibimputed")
 
 ref=readreference(ukb_pcs_200())
 
-dat=readbed("/mnt/storage/private/mrcieu/data/bib/genetic/variants/arrays/imputed/uk10k_1000genomes/released/2017-01-27/data/pcs/hardcalled/mergedhc") # Read "your" data
+dat=readbed(paste0(imputed_data,"data/pcs/hardcalled/mergedhc")) # Read "your" data
 dat=mergeref(dat,ref=ref)     # Merge with the reference (using the included standard reference of 18 UK Biobank Pcs by default)
 dat$mode<-"flashpca"
 pred=predictpcs(dat)  # Predict the first 18 UK Biobank PCs
-writepred("../../data/pcs/output/pcapred/projected.eigenvals_200",dat,pred) # Write output in plink --covar format
+writepred(paste0(imputed_data,"data/pcs/output/pcapred/projected.eigenvals_200"),dat,pred) # Write output in plink --covar format
