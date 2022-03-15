544e9d9ea49b15af24c3151f2e082346ad32b686
Marwa AL Arab <cu20932@bc4login1.bc4.acrc.priv>
2022-03-15 13:44:42 +0000

    added script.sh

--- BiB/GWAS/3.PCs/script.sh
+++ BiB/GWAS/3.PCs/script.sh
@@ -1,4 +1,5 @@
-#Modified by Marwa Al Arab, based on script from Tom Bond.
+#Adapted to bib data by Marwa Al Arab, based on script from Tom Bond.
+#A script to i)generate pcs for 4 stratas in bib data: White mothers, Pakistanis mothers, White Children & Pakistanis Children
 #!/bin/bash
 
 #SBATCH --job-name=pcs_job
@@ -6,52 +7,53 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=4
-
-
-#array=1-22
+#SBATCH --array=1-4
 
 
 #calculate pcs for unrelated individuals, then project all individuals including related individuals onto these pcs
-#run from /mnt/storage/private/mrcieu/data/bib/genetic/variants/arrays/imputed/uk10k_1000genomes/released/2017-01-27/scripts/pcs	
+#change directory to scripts
+cd $bibimputed/scripts/pcs/
 
 #define variables 
-
 plink1='module load apps/plink/1.90'
 plink2='module load apps/plink/2.00'
-
-
-#gen_files=/projects/MRC-IEU/research/data/bib/genetic/variants/arrays/directly_genotyped/released/2020-03-18/data/data
-gen_files=/user/home/cu20932/bib/directly_genotyped/2020-03-18/data/data
-
+eval $bibimputed
+eval $bibdirect
+#directly genotyped data 
+gen_files=$bibdirect/data
 #hardcalled_files = ../../data/pcs/hardcalled/chr${SLURM_ARRAY_TASK_ID}
-#projects/MRC-IEU/research/data/bib/genetic/variants/arrays/directly_genotyped/released/2020-03-18/data/data
-
-out_files=../../data/pcs/output/
+out_files=$bibimputed/scripts/pcs/data/pcs/output/
 #out_path=
 set_file=price_2008_long_range_ld_snps.txt 
 white_mums_ids=included/white.mums.txt
 pakis_mums_ids=included/pakis.mums.txt
 white_child_ids=included/white.child.txt
+pakis_child_ids=included/pakis.child.txt
 
-#check if SLURM_ARRAY_TASK_ID<10
-# if [ ${SLURM_ARRAY_TASK_ID} -gt 9 ]
-# then 
-	# gen_file=${gen_files}data_chr${SLURM_ARRAY_TASK_ID}.vcf.gz
-# else 
-	# gen_file=${gen_files}data_chr0${SLURM_ARRAY_TASK_ID}.vcf.gz
-# fi
+case ${SLURM_ARRAY_TASK_ID} in 
+1)
+ids=white_mums_ids
+;;
+2)
+ids=pakis_mums_ids
+;;
+3)
+ids=white_child_ids
+;;
+4)
+ids=pakis_child_ids
+;;
+*)
+echo "Task Array id not matching case!!"
+esac
 
-# echo $gen_file
 
-#hardcalled_file=${hardcalled_files}/chr${SLURM_ARRAY_TASK_ID}
-#out_file=${out_files}white_mums
-#out_file=${out_files}pakis_mums
-out_file=${out_files}white_child
+out_file=${out_files}ids
 
 
 
 
-
+#This command is to generate hardcalled files to use for merging data with UKB pcs with pcapred
 #to write a set of plink bed/bim/fam files, which contain “hard called” genotype at imputed snps (i.e. integer values representing the most likely imputed genotype).
 #module load apps/plink/2.00
 #time plink --vcf $gen_file dosage=GP --mach-r2-filter 0.82 --hard-call-threshold 0.1 --import-dosage-certainty 0.9 --make-bed --out $hardcalled_file
@@ -60,7 +62,7 @@ eval $plink1
 #module load apps/plink/1.90
 #make .set file with a list of variants in long range ld (from Price, Alkes L., et al. "Long-range LD can confound genome scans in admixed populations." The American Journal of Human Genetics 83.1 (2008): 132-135.)
  time plink --bfile ${gen_files} \
- --keep ${white_child_ids} \
+ --keep ${ids} \
  --make-set ${set_file} \
  --write-set \
  --threads 20 \
@@ -73,7 +75,7 @@ time plink \
 --bfile ${gen_files} \
 --autosome \
 --threads 20 \
---keep ${white_child_ids} \
+--keep ${ids} \
 --king-cutoff 0.04419417 \
 --make-bed \
 --out ${out_file}_unrelated
